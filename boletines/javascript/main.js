d5f838dd0e1efe1b0e68d16863ef8ab37264524d
Savarin <srssdesing@gmail.com>
2023-10-19 00:26:39 +0200

    Agregando el proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,35 @@
+// Obetener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+// Recorrerlos
+
+links.forEach(function(link){
+
+    // Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content= document.querySelector('.content');
+
+
+    // remove class
+    content.classList.remove("animate__animated animate__fadeInDown");
+    content.classList.remove("animate__animated");
+
+    //add
+
+    content.classList.add("animate__animated animate__fadeOutUp")
+    content.classList.add("animate__animated")
+
+    setTimeout(function() {
+        location.href="/";
+
+    }, 600);
+
+    return false;
+        
+    });
+});
+
+
+
