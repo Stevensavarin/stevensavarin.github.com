288c5c19d2437af32c5e76df6d1ac61b1ecc4c50
steven_savarin <srssdesing@gmail.com>
2025-03-16 18:11:09 +0100

    fixing my project

--- wdd231/chamber/scripts/chamber.js
+++ wdd231/chamber/scripts/chamber.js
@@ -17,9 +17,9 @@ const doc = document;
      const response = await fetch(url);
      if (response.ok) {
          const data = await response.json();
-         console.log("RESPONSE data");
-         console.table(data.companies);
- 
+         //console.log("RESPONSE data");
+         //console.table(data.companies);
+         console.log("AWAITING RESPONSE data");
          displayBusinesses(data.companies);
     }
 }   
@@ -27,30 +27,45 @@ const doc = document;
 const displayBusinesses = (companies) => {
      console.log("Going to DISPLAY business data");
      console.table(companies);
- 
+     console.log(`Current grid choise is: ${gridChoice}`);
+
  	cards.innerHTML = "";
      
      companies.forEach((company) => {
-         console.table(company);
+         //console.table(company);
 
-       let card = doc.createElement('section');
-       let h2 = doc.createElement('h2');
-       let web = doc.createElement('p');
-       let phone = doc.createElement('p');
-       let logo = doc.createElement('img');
+        let card = doc.createElement('section');
+        let h2 = doc.createElement('h2');
+        let level = doc.createElement('p');
+        let address = doc.createElement('p');
+        let web = doc.createElement('p');
+        let phone = doc.createElement('p');
+        let logo = doc.createElement('img');
    
-
        h2.innerHTML = `${company.name}`;
+       h2.setAttribute("class", "buss_name");
+       if (company.memberLevel == 3) {
+            level.innerHTML = `Gold Level - &#x2605&#x2605&#x2605`;
+       } else if (company.memberLevel == 2) {
+            level.innerHTML = `Silver Level - &#x2605&#x2605`;           
+       }
+    address.innerHTML = `${company.address}`;
+    address.setAttribute("class", "addr");
        web.innerHTML = `${company.url}`;
+       web.setAttribute("class", "web_url");
        phone.innerHTML = `${company.phoneNumber}`
-
+       phone.setAttribute("class", "phone");
+       
        logo.setAttribute('src', company.imageurl);
        logo.setAttribute('alt', `logo of ${company.name}`);
        logo.setAttribute('loading', 'lazy');
-       logo.setAttribute('width', '140');
-       logo.setAttribute('height', 'auto');
+       //logo.setAttribute('width', '140'); // to check
+       //logo.setAttribute('height', 'auto'); // to check
+       logo.setAttribute('class', 'business-logo'); // to check
    
        card.appendChild(h2);
+       //card.appendChild(level);  // to do
+       //card.appendChild(address); // to do
        card.appendChild(web);
        card.appendChild(phone);  
        card.appendChild(logo);
@@ -58,4 +73,21 @@ const displayBusinesses = (companies) => {
        cards.appendChild(card);
     });
 }
+ 
+const gridButton = doc.querySelector('#select_grid');
+const listButton = doc.querySelector('#select_list');
+ 
+let gridChoice = "grid";
+console.log("DEFAULT: grid");
+ 
+gridButton.addEventListener('click', () => {
+    console.log("gridButton clicked");
+    gridChoice = "grid";
+    cards.setAttribute('class', gridChoice);
+});
 
+listButton.addEventListener('click', () => {
+    console.log("listButton clicked");
+    gridChoice = "list";
+    cards.setAttribute('class', gridChoice);
+});
